c17fc78301c1ac51c65d5545374fde02ebe8b51a
Gonzalo Diez <gdiezpa@gmail.com>
2025-01-05 17:33:48 +0100

    feat: add global variable __collections__ for collection management

--- framework/global.d.ts
+++ framework/global.d.ts
@@ -1,6 +1,13 @@
+import { CollectionIndexModule, CollectionModule } from './types.ts';
+
 export {};
 
 declare global {
   // deno-lint-ignore no-var
   var BUILD_TIME: boolean;
+  // deno-lint-ignore no-var
+  var __collections__: Record<string, {
+    index: CollectionIndexModule;
+    entries: Record<string, CollectionModule>;
+  }>;
 }
